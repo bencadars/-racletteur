45916089e68f87992920f1fe4e19baa66a1f5ea5
fouad bamhammmed <fouad@bamhammed.com>
2023-06-19 17:26:49 +0200

    branch_machine

---
+++ app/controllers/machines_controller.rb
@@ -0,0 +1,24 @@
+class MachinesController < ApplicationController
+  def index
+    @machines = Machine.all
+  end
+
+  def new
+    @machine = Machine.new
+  end
+
+  def create
+    @machine = Machine.new(machine_params)
+    @machine.user_id = current_user.id
+    @machine.save!
+    redirect_to machines_index_path
+  end
+
+  private
+
+  def machine_params
+    params.require(:machine).permit(:type, :capacity, :power, :brand, :function, :price, :user_id)
+  end
+
+end
+end
