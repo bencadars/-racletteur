5ded8cb9a2ec5800927aa6167894b15bf89b5082
benjamin cadars <benjamin.cadars@gmail.com>
2023-06-19 17:49:18 +0200

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :machines
   has_many :bookings
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :phone_number, presence:true
+  validates :address, presence:true
 end
